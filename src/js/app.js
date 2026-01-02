cf7811805acec25f2e6deb47a797867562593cb5
Paweł Dubowik <p.dubowik27@gmail.com>
2026-01-02 13:33:44 +0100

    Finish styling Search and Discover sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,38 @@
-import { select } from "./settings.js";
+import { select } from './settings.js';
 
 
 
 
 const app = {
 
-    getElements: function(){
-        const thisApp = this;
+  getElements: function(){
+    const thisApp = this;
 
-        thisApp.dom = {};
+    thisApp.dom = {};
 
-        thisApp.dom.wrapper = document;
+    thisApp.dom.wrapper = document;
 
-        thisApp.dom.songContainers = thisApp.dom.wrapper.querySelectorAll(select.containerOf.songPlayer);
+    thisApp.dom.songContainers = thisApp.dom.wrapper.querySelectorAll(select.containerOf.songPlayer);
 
 
-    },
+  },
 
-    initPlayers: function(container){
-        const thisApp = this;
+  initPlayers: function(container){
+    const thisApp = this;
 
-        for(let player of container){
-            new GreenAudioPlayer(player, {
-                stopOthersOnPlay: true,
-            });
-        }
-    },
-
-    init: function(){
-        const thisApp = this;
-
-        thisApp.getElements();
-        thisApp.initPlayers(thisApp.dom.songContainers);
+    for(let player of container){
+      new GreenAudioPlayer(player, {
+        stopOthersOnPlay: true,
+      });
     }
-}
+  },
+
+  init: function(){
+    const thisApp = this;
+
+    thisApp.getElements();
+    thisApp.initPlayers(thisApp.dom.songContainers);
+  }
+};
 
 app.init();
